ab1805ece34e6d78117b9d049a74ae3fe9b9eece
mt19146 <57112474+mt19146@users.noreply.github.com>
2023-02-08 14:50:14 +0530

    Update SimFxnLength.m

--- Line Length Measure and Density/SimFxnLength.m
+++ Line Length Measure and Density/SimFxnLength.m
@@ -4,17 +4,27 @@ function out = SimFxnLength(gamma_vec,iterations,nB,xt,R,t)
         r_vec = R * rand(iterations,nB);
         theta_vec = 2*pi*rand(iterations,nB);
         line_d_vec = abs(xt*cos(theta_vec) - r_vec);
-        ind = arrayfun(@(i) (line_d_vec(i,:)<=t),1:iterations,'UniformOutput',false);
+        ind1 = arrayfun(@(i) (line_d_vec(i,:)<=t(1)),1:iterations,'UniformOutput',false);
+        ind2 = arrayfun(@(i) (line_d_vec(i,:)<=t(2)),1:iterations,'UniformOutput',false);
         temp = [];
         for iter = 1:iterations
-            ind2 = find(ind{iter}==1);
-            if ~isempty(ind2)
-                r = line_d_vec(iter,ind2);
-                distance = 2*sqrt(t^2 - r.^2);
-                temp = [temp sum(distance)];
+            ind11 = find(ind1{iter}==1);
+            ind22 = find(ind2{iter}==1);
+            if ~isempty(ind11)
+                r = line_d_vec(iter,ind11);
+                distance = 2*sqrt(t(1)^2 - r.^2);
+                temp1 = sum(distance);
             else
-                temp = [temp 0];
+                temp1 = 0;
             end
+            if ~isempty(ind22)
+                r = line_d_vec(iter,ind22);
+                distance = 2*sqrt(t(2)^2 - r.^2);
+                temp2 = sum(distance);
+            else
+                temp2 = 0;
+            end
+            temp = [temp temp2-temp1];
         end
     end
     out = temp;
